7ee8962e7f295e45b2643957cf7bd1cb35df068c
Oleg Grenrus <oleg.grenrus@iki.fi>
2024-03-04 14:10:18 +0200

    Elab

--- src/PoriTT/Elab/Ctx.hs
+++ src/PoriTT/Elab/Ctx.hs
@@ -1,1 +1,121 @@
-module PoriTT.Elab.Ctx () where
+module PoriTT.Elab.Ctx (
+    ElabCtx (..),
+    toLintCtx,
+    bind,
+    bind',
+    emptyElabCtx,
+) where
+
+import PoriTT.Base
+import PoriTT.Name
+import PoriTT.Value
+import PoriTT.Term
+import PoriTT.Rigid
+import PoriTT.Stage
+import PoriTT.Lint
+import PoriTT.PP
+import PoriTT.Loc
+
+-------------------------------------------------------------------------------
+-- Elaboration context
+-------------------------------------------------------------------------------
+
+-- | Elaboration context.
+--
+data ElabCtx ctx ctx' = ElabCtx
+    { names  :: !(Env ctx Name)
+    , names' :: !(Env ctx' Name)
+    , nscope :: !NameScope
+    , values :: !(EvalEnv HasMetas ctx ctx')
+    , types  :: !(Env ctx (VTerm HasMetas ctx'))
+    , types' :: !(Env ctx' (VTerm HasMetas ctx'))
+    , rigids :: !(RigidMap ctx' (VTerm HasMetas ctx'))
+    , stages :: !(Env ctx Stage)
+    , cstage :: !Stage
+    , size   :: !(Size ctx')
+    , wk     :: Wk ctx' ctx               -- ^ weakening from target context to source context. This is possible, as target context is the same except the let bound values are missing.
+    , loc    :: !(Loc)
+    , doc    :: ![Doc]
+    }
+
+-- | Empty elaboration context.
+--
+-- Needs global 'NameScope' for pretty-printing.
+--
+emptyElabCtx :: NameScope -> ElabCtx EmptyCtx EmptyCtx
+emptyElabCtx ns = ElabCtx
+    { names  = EmptyEnv
+    , names' = EmptyEnv
+    , nscope = ns
+    , values = EmptyEnv
+    , types  = EmptyEnv
+    , types' = EmptyEnv
+    , rigids = emptyRigidMap
+    , stages = EmptyEnv
+    , cstage = stage0
+    , size   = SZ
+    , wk     = IdWk
+    , loc    = startLoc "<unknown>"
+    , doc    = []
+    }
+
+toLintCtx :: ElabCtx ctx ctx' -> LintCtx ctx ctx'
+toLintCtx ctx = LintCtx
+    { values = ctx.values
+    , types  = ctx.types
+    , types' = ctx.types'
+    , rigids = ctx.rigids
+    , stages = ctx.stages
+    , cstage = ctx.cstage
+    , names  = ctx.names
+    , names' = ctx.names'
+    , nscope = ctx.nscope
+    , size   = ctx.size
+    , doc    = ctx.doc
+    }
+
+bind
+    :: ElabCtx ctx ctx'
+    -> Name                     -- ^ term name
+    -> Name                     -- ^ name in types
+    -> VTerm NoMetas ctx'       -- ^ type
+    -> ElabCtx (S ctx) (S ctx')
+bind (ElabCtx xs xs' ns v ts ts' rs ss cs s wk l pp) x x' a = ElabCtx
+    { names   = xs :> x
+    , names'  = xs' :> x'
+    , nscope  = ns
+    , values  = mapSink v :> t
+    , types   = mapSink ts :> sink a
+    , types'  = mapSink ts' :> sink a
+    , rigids  = rigidMapSink (mapSink rs)
+    , stages  = ss :> cs
+    , cstage  = cs
+    , size    = SS s
+    , wk      = KeepWk wk
+    , loc     = l
+    , doc     = pp
+    }
+  where
+    t = evalZ s
+
+bind'
+    :: ElabCtx ctx ctx'
+    -> Name                 -- ^ variable name
+    -> EvalElim NoMetas ctx'   -- ^ value
+    -> VTerm NoMetas ctx'   -- ^ type
+    -> ElabCtx (S ctx) ctx'
+bind' (ElabCtx xs xs' ns v ts ts' rs ss cs s wk l pp) x t a = ElabCtx
+    { names   = xs :> x
+    , names'  = xs'
+    , nscope  = ns
+    , values  = v :> t
+    , types   = ts :> a
+    , types'  = ts'
+    , rigids  = rs
+    , stages  = ss :> cs
+    , cstage  = cs
+    , size    = s
+    , wk      = SkipWk wk
+    , loc     = l
+    , doc     = pp
+    }
--- src/PoriTT/Elab/Monad.hs
+++ src/PoriTT/Elab/Monad.hs
@@ -1,2 +1,19 @@
-module PoriTT.Elab.Monad () where
+module PoriTT.Elab.Monad (
+    ElabM,
+    newRigid,
+) where
 
+import PoriTT.Base
+import PoriTT.PP
+import PoriTT.Rigid
+import PoriTT.ExceptState
+import PoriTT.Value
+import PoriTT.Term
+import PoriTT.Elab.Ctx
+
+type ElabM = ExceptState Doc RigidState
+
+newRigid :: ElabCtx ctx ctx' -> VTerm HasMetas ctx' -> ElabM (ElabCtx ctx ctx', RigidVar ctx')
+newRigid ctx ty = do
+    r <- takeRigidVar
+    return (ctx { rigids = insertRigidMap r ty ctx.rigids }, r)
