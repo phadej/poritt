abaa80c16ada29aa1609522be4aa2c4f0c2b4312
Oleg Grenrus <oleg.grenrus@iki.fi>
2024-03-25 22:53:37 +0200

    Pruning

---
+++ src/PoriTT/Pruning.hs
@@ -0,0 +1,21 @@
+module PoriTT.Pruning (
+    Pruning (..),
+    weakenPruning,
+    emptyPruning,
+) where
+
+import PoriTT.Base
+
+data Pruning ctx where
+    Pruning :: Wk ctx ctx' -> Pruning ctx'
+
+deriving instance Show (Pruning ctx)
+
+weakenPruning :: Wk n m -> Pruning n -> Pruning m
+weakenPruning w (Pruning xs) = Pruning (compWk xs w)
+
+emptyPruning :: Size ctx -> Pruning ctx
+emptyPruning = Pruning . go where
+    go :: Size ctx -> Wk EmptyCtx ctx
+    go SZ     = IdWk
+    go (SS s) = SkipWk (go s)
